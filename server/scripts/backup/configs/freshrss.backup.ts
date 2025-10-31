1d57b9880bfa3d5297ebf72385fa4dd3d245b47a
Anton Shubin <2spy4x@gmail.com>
2025-11-01 03:21:45 +0800

    feat(server): add FreshRSS service

---
+++ server/scripts/backup/configs/freshrss.backup.ts
@@ -0,0 +1,11 @@
+import { BackupConfig } from "../src/+lib.ts"
+
+const backupConfig: BackupConfig = {
+  name: "freshrss",
+  sourcePaths: "default",
+  containers: {
+    stop: "default",
+  },
+}
+
+export default backupConfig
